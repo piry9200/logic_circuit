e6f954c79b1ff1c6d5cd974f57b71e44cd8a9880
piry9200 <ymt104ro09@gmail.com>
2023-05-09 17:52:45 +0900

    add: 起動時のイベントのセットをする、firstSettingを追加

--- js/observeCircuit.js
+++ js/observeCircuit.js
@@ -9,7 +9,6 @@ let lines = {}; //グローバル　回路を結ぶ線を保持
 //MutationObserver（インスタンス）の作成
 var mo = new MutationObserver(function(record, observer) { //変化した際の処理を記述
 
-
     //以下の処理は、追加された論理ゲートをcircuit内で移動できるようにするための処理。追加されるたびに設定し直す。
     let gates = document.getElementsByClassName("gate_parent");
     let draggable = [];
@@ -46,6 +45,7 @@ var mo = new MutationObserver(function(record, observer) { //変化した際の�
         if (button.dataset.event) { //addEventListnerの重複を回避
             continue;
         }
+
         button.dataset.event = true //イベントが登録されいることを示す
         button.addEventListener("click", (event) => { //クリックされたら、そのオブジェクトをピンクにすると同時に、回路内に他にピンクのやつがいないかどうかを判定
             if(event.target.style.backgroundColor != "pink"){ //クリックされたボタンがピンク色じゃない場合ピンクにする
@@ -201,6 +201,155 @@ function setLines(startElement, endElement){ //
         LeaderLine.pointAnchor(endElement, {x:"8px", y:"8px"})
     );
     return line_object;
+
+}
+
+function firstSetting(){
+
+let buttons = document.getElementsByClassName("ioButton");
+    
+    for(let button of buttons){ //クリックで背景色をピンクにするイベントを設置
+        if (button.dataset.event) { //addEventListnerの重複を回避
+            continue;
+        }
+
+        button.dataset.event = true //イベントが登録されいることを示す
+        button.addEventListener("click", (event) => { //クリックされたら、そのオブジェクトをピンクにすると同時に、回路内に他にピンクのやつがいないかどうかを判定
+            if(event.target.style.backgroundColor != "pink"){ //クリックされたボタンがピンク色じゃない場合ピンクにする
+                event.target.style.backgroundColor = "pink";
+                chosen_buttons.push(event.target);
+            }
+
+            if(event.target.dataset.button_type == "output"){ //outputボタンが選択されたときに実行。入力されている信号から出力用の信号を計算する。
+                switch(event.target.parentNode.dataset.type){
+                    case "AND":
+                        let answer1 = [] //計算結果を入れる用
+                        if(event.target.parentNode.dataset.input1 != undefined && event.target.parentNode.dataset.input1 != "undefined" &&
+                        event.target.parentNode.dataset.input1 != null && event.target.parentNode.dataset.input2 != undefined && event.target.parentNode.dataset.input2 != "undefined" &&
+                        event.target.parentNode.dataset.input2 != null)
+                        {//input1 input2が定義されていない（undefined,null)ときは実行しない
+                            console.log("calc!");
+                            for(let i=0; i<event.target.parentNode.dataset.input1.length; i=i+2){
+                                answer1.push(event.target.parentNode.dataset.input1[i] * event.target.parentNode.dataset.input2[i]);
+                            }
+                            event.target.dataset.timechart = answer1; //outputボタンに計算結果(timechart)を設定
+                        }
+                        break
+                    case "OR":
+                        let answer2 = [] //計算結果を入れる用
+                        if(event.target.parentNode.dataset.input1 != undefined && event.target.parentNode.dataset.input1 != "undefined" &&
+                        event.target.parentNode.dataset.input1 != null && event.target.parentNode.dataset.input2 != undefined && event.target.parentNode.dataset.input2 != "undefined" &&
+                        event.target.parentNode.dataset.input2 != null)
+                        {//input1 input2が定義されていない（undefined,null)ときは実行しない
+                            for(let i=0; i<event.target.parentNode.dataset.input1.length; i=i+2){
+                                let num = event.target.parentNode.dataset.input1[i] + event.target.parentNode.dataset.input2[i];
+                                answer2.push( num >= 1 ? 1 : 0); //二つのインプットの和が１以上だったら１をプッシュ
+                            }
+                            event.target.dataset.timechart = answer2; //outputボタンに計算結果(timechart)を設定
+                        }
+                        break;
+                    case "NOT":
+                        let answer3 = [] //計算結果を入れる用
+                        if(event.target.parentNode.dataset.input1 != undefined && event.target.parentNode.dataset.input1 != "undefined" &&
+                        event.target.parentNode.dataset.input1 != null)
+                        {//input1 が定義されていない（undefined,null)ときは実行しない
+                            for(let i=0; i<event.target.parentNode.dataset.input1.length; i=i+2){
+                                let num = event.target.parentNode.dataset.input1[i]
+                                answer3.push( num == 1 ? 0 : 1); //インプットが１だったら0,0だったら1をプッシュ
+                            }
+                            event.target.dataset.timechart = answer3; //outputボタンに計算結果(timechart)を設定
+                        }
+                        break;
+                }
+            }
+
+            if(chosen_buttons.length == 2){ //二つ選択されたときに実行。選択されている二つのゲートに対しての処理
+                let leftside_Xcoordinate = chosen_buttons[0].getBoundingClientRect().left;
+                if(leftside_Xcoordinate > chosen_buttons[1].getBoundingClientRect().left){ //chosen_buttonsの[0]の方が左側にあるようにする
+                    let temp = chosen_buttons[1];
+                    chosen_buttons[1] = chosen_buttons[0];
+                    chosen_buttons[0] = temp;
+                }
+                //左(timechart)から右( input(1 or 2) )に代入
+                chosen_buttons[1].dataset.input = chosen_buttons[0].dataset.timechart; //inputボタンに信号を代入
+                //---------------↓gate_parentのdatasetにinput1,input2として保持させる-------------
+                if(chosen_buttons[1].dataset.button_type == "input1"){ //input1ボタンからgate_parentのdataset.input1に代入
+                    console.log("hiiiii");
+                    chosen_buttons[1].parentNode.dataset.input1 = chosen_buttons[1].dataset.input;
+                }else if(chosen_buttons[1].dataset.button_type == "input2"){ //input2ボタンからgate_parentのdataset.input2に代入
+                    console.log("uiiiii");
+                    chosen_buttons[1].parentNode.dataset.input2 = chosen_buttons[1].dataset.input;
+                }else if(chosen_buttons[1].parentNode.id == "output"){
+                    chosen_buttons[1].parentNode.dataset.input = chosen_buttons[1].dataset.input;
+                }
+                //------------------------------------------------------------------------------
+                for(let chosen_button of chosen_buttons){ //色を透明に戻す
+                    chosen_button.style.backgroundColor = "";
+                }
+                //線を引いて、引いた線に関わるオブジェクトたちに線の情報をセット
+                let line = setLines(chosen_buttons[0], chosen_buttons[1]);
+                let line_id = line._id;
+                lines[line_id] = line;
+                chosen_buttons[0].dataset.line_start = line_id;
+                chosen_buttons[1].dataset.line_end = line_id;
+                //-----------------gate_parentに接続中の線の情報を送る-----------------------
+                switch(chosen_buttons[0].parentNode.dataset.type){
+                    case "AND":
+                    case "OR":
+                        if(chosen_buttons[0].parentNode.dataset.connecting1 == undefined || chosen_buttons[0].parentNode.dataset.connecting1 == "undefined" ||
+                        chosen_buttons[0].parentNode.dataset.connecting1 == null){ //parentNode.dataset.connecting1が定義されいないときに実行
+                            chosen_buttons[0].parentNode.dataset.connecting1 = chosen_buttons[0].dataset.line_start
+                        }else if(chosen_buttons[0].parentNode.dataset.connecting2 == undefined || chosen_buttons[0].parentNode.dataset.connecting2 == "undefined" ||
+                        chosen_buttons[0].parentNode.dataset.connecting2 == null){ //parentNode.dataset.connecting2が定義されいないときに実行
+                            chosen_buttons[0].parentNode.dataset.connecting2 = chosen_buttons[0].dataset.line_start
+                        }else{
+                            chosen_buttons[0].parentNode.dataset.connecting3 = chosen_buttons[0].dataset.line_start
+                        }
+                        break;
+                    case "NOT":
+                        if(chosen_buttons[0].parentNode.dataset.connecting1 == undefined || chosen_buttons[0].parentNode.dataset.connecting1 == "undefined" ||
+                        chosen_buttons[0].parentNode.dataset.connecting1 == null){ //parentNode.dataset.connecting1が定義されいないときに実行
+                            chosen_buttons[0].parentNode.dataset.connecting1 = chosen_buttons[0].dataset.line_start
+                        }else{
+                            chosen_buttons[0].parentNode.dataset.connecting2 = chosen_buttons[0].dataset.line_start
+                        }
+                        break;
+                    default: break;
+                }
+                // ↑↑↑↑↑結線の左側に対して処理　        結線の右側に対して処理↓↓↓↓↓↓↓↓
+                switch(chosen_buttons[1].parentNode.dataset.type){
+                    case "AND":
+                    case "OR":
+                        if(chosen_buttons[1].parentNode.dataset.connecting1 == undefined || chosen_buttons[1].parentNode.dataset.connecting1 == "undefined" ||
+                        chosen_buttons[1].parentNode.dataset.connecting1 == null){ //parentNode.dataset.connecting1が定義されいないときに実行
+                            console.log("deketeruyooo");
+                            chosen_buttons[1].parentNode.dataset.connecting1 = chosen_buttons[1].dataset.line_end;
+                        }else if(chosen_buttons[1].parentNode.dataset.connecting2 == undefined || chosen_buttons[1].parentNode.dataset.connecting2 == "undefined" ||
+                        chosen_buttons[1].parentNode.dataset.connecting2 == null){ //parentNode.dataset.connecting2が定義されいないときに実行
+                            chosen_buttons[1].parentNode.dataset.connecting2 = chosen_buttons[1].dataset.line_end;
+                        }else{
+                            chosen_buttons[1].parentNode.dataset.connecting3 = chosen_buttons[1].dataset.line_end;
+                        }
+                        break;
+                    case "NOT":
+                        if(chosen_buttons[1].parentNode.dataset.connecting1 == undefined || chosen_buttons[1].parentNode.dataset.connecting1 == "undefined" ||
+                        chosen_buttons[1].parentNode.dataset.connecting1 == null){ //parentNode.dataset.connecting1が定義されいないときに実行
+                            chosen_buttons[1].parentNode.dataset.connecting1 = chosen_buttons[1].dataset.line_end;
+                        }else{
+                            chosen_buttons[1].parentNode.dataset.connecting2 = chosen_buttons[1].dataset.line_end;
+                        }
+                    default: break;
+                //------------↑↑↑↑↑↑gate_parentに接続中の線の情報を送る↑↑↑↑↑↑----------------------------------------
+
+                }
+
+                chosen_buttons.splice(0) //すべての要素を削除
+            }
+        })
+
+    }
+
+
 
 }
 
